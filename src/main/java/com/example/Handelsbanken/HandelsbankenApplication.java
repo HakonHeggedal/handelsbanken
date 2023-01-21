2cda5e081e1c5a2ad4ac6b8529cd91b3f157626a
håkon <hakonheggedal@gmail.com>
2023-01-21 17:58:26 +0100

    initial

---
+++ src/main/java/com/example/Handelsbanken/HandelsbankenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Handelsbanken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandelsbankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandelsbankenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Handelsbanken/HandelsbankenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Handelsbanken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandelsbankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
